d3920bfe05e024f10b67d7f951ff3ee2308f6d6d
MarianaTamiAsanoNagase <a2020114879@estudiantes.upsa.edu.bo>
2021-10-11 15:01:37 -0400

    Agregar archivos de proyecto.

---
+++ PersonaWeb/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonaWeb.Models
+{
+    public enum SexType 
+    {
+        Female =0,
+        Male =1,
+    }
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+        [Required]
+        [StringLength(60,MinimumLength = 5,ErrorMessage = "Por favor, ingrese entre 5 a 60 caracteres")]
+        [Display(Name = "Nombre de la persona")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Sexo")]
+        public SexType Sex { get; set; }
+        [Display(Name = "Pasa tiempo")]
+        public string Hobby { get; set; }
+    }
+}
